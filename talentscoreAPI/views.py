e946a9d9a259e44558e33adfad0e64d44523dcbb
Apple Macbook Air M1 <applemacbookairm1@Apples-MacBook-Air.local>
2023-06-19 16:24:31 +0400

    changes

--- talentscoreAPI/views.py
+++ talentscoreAPI/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 from talentscoreAPI.models import Answers, Questions, SubStage
 from talentscoreAPI.serializers import (AnswerSerializer, QuestionSerializer,
@@ -20,12 +21,16 @@ class QuestionListView(generics.ListAPIView):
     serializer_class = QuestionSerializer
 
     def get(self, request, slug=None, id=None):
+        
         if slug and id:
-            questions = Questions.objects.filter(
-                slug=slug, question_depends_answer__id=id
-            )
+            questions = Questions.objects.filter(slug=slug, question_depends_answer__id=id)
+            if not questions.exists():
+                next_stage_questions = Questions.objects.filter(substage__substage='Olympiad Questions')
+                serializer = QuestionSerializer(next_stage_questions, many=True)
+                return Response(serializer.data)
             serializer = QuestionSerializer(questions, many=True)
             return Response(serializer.data)
+           
         elif slug:
             # Filter questions based on the provided slug
             questions = Questions.objects.filter(slug=slug)
